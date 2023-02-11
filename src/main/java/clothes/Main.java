e179d0d8c671e48f14cbcb0acfd296913c92cd46
KevinH <kevinharjo1@gmail.com>
2023-02-11 12:21:11 +0200

    Commit 2 "Hopefully code can be seen now"

--- src/main/java/clothes/Main.java
+++ src/main/java/clothes/Main.java
@@ -1,2 +1,47 @@
-package clothes;public class Main {
+package clothes;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Pocket pocket = new Pocket(0);
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        int amount;
+
+        do {
+            System.out.println("1. Add money");
+            System.out.println("2. Withdraw money");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the amount of money to add: ");
+                    amount = scanner.nextInt();
+                    pocket.setMoney(amount);
+                    break;
+                case 2:
+                    System.out.print("Enter the amount of money to withdraw: ");
+                    amount = scanner.nextInt();
+                    int money = pocket.getMoney();
+                    if (money < amount) {
+                        System.out.println("Not enough money.");
+                    } else {
+                        pocket.setMoney(money - amount);
+                        System.out.println("Withdrawn " + amount + " from the pocket.");
+                    }
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        } while (choice != 3);
+
+        scanner.close();
+
+    }
 }
--- src/main/java/clothes/Pocket.java
+++ src/main/java/clothes/Pocket.java
@@ -1,2 +1,28 @@
-package clothes;public class Pocket {
+package clothes;
+
+public class Pocket {
+    private int money;
+
+    public Pocket(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        if (money <= 10) {
+            return 0;
+        } else {
+            return money;
+        }
+    }
+
+    public void setMoney(int money) {
+        if (money < 0 || money > 3000) {
+            System.out.println("I don’t have enough space in my pocket for as much money!");
+
+        } else {
+            this.money = money;
+        }
+    }
 }
+
+
